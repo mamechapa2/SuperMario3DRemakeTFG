34ab447b53db867cb8a1af8e686bfc39d4d19f9c
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-06-11 19:32:50 +0200

    Mejoras para la camara
    Limitar el movimiento arriba y abajo e invertir el eje y

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,6 +11,12 @@ public class CameraController : MonoBehaviour
 
     public float rotationSpeed;
     public Transform rotationPoint;
+
+    public float maxCameraAngle = 65f;
+    public float minCameraAngle = -60f;
+
+    public bool invertYAxis = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -34,7 +40,25 @@ public class CameraController : MonoBehaviour
 
         //Girar el punto de rotacion para que el jugador no rote en el eje X
         float vertical = Input.GetAxis("Mouse Y") * rotationSpeed;
-        rotationPoint.transform.Rotate(-vertical, 0, 0);
+        if (invertYAxis)
+        {
+            rotationPoint.transform.Rotate(vertical, 0, 0);
+        }
+        else
+        {
+            rotationPoint.transform.Rotate(-vertical, 0, 0);
+        }
+
+        //Limitar el movimiento de la camara arriba y abajo
+        if(rotationPoint.transform.rotation.eulerAngles.x > maxCameraAngle && rotationPoint.transform.rotation.eulerAngles.x < 180f)
+        {
+            rotationPoint.transform.rotation = Quaternion.Euler(maxCameraAngle, 0, 0);
+        }
+
+        if (rotationPoint.transform.rotation.eulerAngles.x > 180f && rotationPoint.transform.rotation.eulerAngles.x < 360f + minCameraAngle)
+        {
+            rotationPoint.transform.rotation = Quaternion.Euler(360f + minCameraAngle, 0, 0);
+        }
 
         //Mover la camara
         float desiredYAngle = player.transform.eulerAngles.y;
@@ -43,11 +67,13 @@ public class CameraController : MonoBehaviour
         Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
         transform.position = player.transform.position - (rotation * offset);
 
+        //Bloquear la camara para que no baje de la mitaad del jugador
         if(transform.position.y < player.transform.position.y)
         {
             transform.position = new Vector3(transform.position.x, player.transform.position.y - 0.5f, transform.position.z);
         }
+
         //transform.position = (player.transform.position - offset);
-        transform.LookAt(player.transform);
+        transform.LookAt(player.transform); //Mirar hacia el jugador
     }
 }
