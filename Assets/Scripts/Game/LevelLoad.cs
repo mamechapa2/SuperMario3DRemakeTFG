ad73ffb5efca84d0f059f479a813f9b7ad3b5fea
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-06-21 19:24:01 +0200

    Level1-1 preload

---
+++ Assets/Scripts/Game/LevelLoad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(loadLevel());
+    }
+
+    private IEnumerator loadLevel()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
