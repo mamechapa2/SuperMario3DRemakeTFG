48e121ba20238ede644f43975d3fbcf5ccd60d74
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-06-24 12:40:15 +0200

    Mas bugfixes

--- Assets/Scripts/Game/CameraController3D.cs
+++ Assets/Scripts/Game/CameraController3D.cs
@@ -8,13 +8,14 @@ public class CameraController3D : MonoBehaviour
 
     public float offsetX = 0;
     public float offsetY = 0;
+    public float offsetZ = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("PlayerCC").gameObject;
-        transform.position = new Vector3(player.transform.position.x - offsetX, player.transform.position.y + offsetY, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x - offsetX, player.transform.position.y + offsetY, player.transform.position.z - offsetZ);
 
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -22,6 +23,6 @@ public class CameraController3D : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x - offsetX, player.transform.position.y + offsetY, transform.position.z);
+        transform.position = new Vector3(player.transform.position.x - offsetX, player.transform.position.y + offsetY, player.transform.position.z - offsetZ);
     }
 }
