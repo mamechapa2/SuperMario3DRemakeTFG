2e3a5ef216e10c77f558dc79c868b9a5382a2c81
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-06-26 13:28:44 +0200

    Enemigo koopa terminado

---
+++ Assets/Scripts/Enemy/Koopa/KoopaLeftTrigger.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoopaLeftTrigger : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool goRight = false;
+    public GameObject rightTrigger;
+
+    public float moveSpeed = 18;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goRight)
+        {
+            rb.velocity = new Vector3(-1 * moveSpeed, -9, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        switch (other.tag)
+        {
+            case "Enemy":
+                Destroy(other.transform.parent.transform.parent.gameObject);
+                break;
+            case "Player":
+                goRight = true;
+                rightTrigger.GetComponent<KoopaRightTrigger>().goLeft = false;
+                break;
+            default:
+                goRight = true;
+                rightTrigger.GetComponent<KoopaRightTrigger>().goLeft = false;
+                break;
+
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/Koopa/KoopaRightTrigger.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoopaRightTrigger : MonoBehaviour
+{
+    public Rigidbody rb;
+    public bool goLeft = false;
+    public GameObject leftTrigger;
+
+
+    public float moveSpeed = 18;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goLeft)
+        {
+            rb.velocity = new Vector3(1 * moveSpeed, -9, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Enemy":
+                Destroy(other.transform.parent.transform.parent.gameObject);
+                break;
+            case "Player":
+                goLeft = true;
+                leftTrigger.GetComponent<KoopaLeftTrigger>().goRight = false;
+                break;
+            default:
+                goLeft = true;
+                leftTrigger.GetComponent<KoopaLeftTrigger>().goRight = false;
+                break;
+
+        }
+    }
+}
