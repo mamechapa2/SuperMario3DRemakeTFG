9958c408badbbc39f4d5b35446dab36916c7cf25
Alfonso Jose Arroyo Loaisa <alfonsoarroyoloaisa@gmail.com>
2021-07-24 19:16:26 +0200

    Mas bugfixes :(

--- Assets/Scripts/Game/ActivateEndDialog.cs
+++ Assets/Scripts/Game/ActivateEndDialog.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 public class ActivateEndDialog : MonoBehaviour
 {
     public GameObject dialogObject;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter(Collider other)
     {
         dialogObject.SetActive(true);
