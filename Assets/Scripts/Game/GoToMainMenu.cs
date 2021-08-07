9f4c98721ca876f7437d853b0a34507c8d6f6750
Alfonso Jose Arroyo Loaisa <alfonsoarroyoloaisa@gmail.com>
2021-08-07 19:40:52 +0200

    Mejoras para el menu principal

--- Assets/Scripts/Game/GoToMainMenu.cs
+++ Assets/Scripts/Game/GoToMainMenu.cs
@@ -6,6 +6,16 @@ public class GoToMainMenu : MonoBehaviour
 {
     public GameObject mainMenuObject;
     public GameObject mainMenuMusic;
+
+    private void Update()
+    {
+        if (Input.anyKey)
+        {
+            this.gameObject.SetActive(false);
+            mainMenuObject.SetActive(true);
+            mainMenuMusic.SetActive(true);
+        }
+    }
     public void goToMainMenu()
     {
         this.gameObject.SetActive(false);
