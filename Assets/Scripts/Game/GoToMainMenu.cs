e868ebf5695d4d61b6125fdb4a64e9c95e5f85bb
Alfonso Jose Arroyo Loaisa <alfonsoarroyoloaisa@gmail.com>
2021-08-07 19:28:36 +0200

    Musica para el menu principal

--- Assets/Scripts/Game/GoToMainMenu.cs
+++ Assets/Scripts/Game/GoToMainMenu.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GoToMainMenu : MonoBehaviour
 {
     public GameObject mainMenuObject;
+    public GameObject mainMenuMusic;
     public void goToMainMenu()
     {
         this.gameObject.SetActive(false);
         mainMenuObject.SetActive(true);
+        mainMenuMusic.SetActive(true);
     }
 
 }
